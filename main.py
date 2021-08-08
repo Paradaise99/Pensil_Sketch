40606e02b25b832ed0073362a8be339d5397a023
Paradaise99 <44893954+Paradaise99@users.noreply.github.com>
2021-08-08 10:15:19 +0100

    Rename the file and first test complited

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import cv2
 
-img_location = './img'
+img = cv2.imread('img/both.jpeg')
 
-filename = 'both.jpeg'
-
-img = cv2.imread(img_location + filename)
-
-gray_img = cv2.imread(img, cv2.COLOR_BGR2GRAY)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 inverted_gray_img = 255 - gray_img
 
@@ -16,6 +12,7 @@ inverted_blurred_img = 255 - blurred_img
 
 pencil_img = cv2.divide(gray_img, inverted_blurred_img, scale = 256.0)
 
+
 cv2.imshow('Pencil Sketch', pencil_img)
 
-cv2.waitKey('q')
+cv2.waitKey(0)
